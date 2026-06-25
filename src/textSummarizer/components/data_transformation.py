181324624a4d46688b0759491f536004301a2b3b
Rucha9 <ruchabhale92@gmail.com>
2026-06-24 22:39:26 -0700

    model trainer added

--- src/textSummarizer/components/data_transformation.py
+++ src/textSummarizer/components/data_transformation.py
@@ -24,6 +24,6 @@ class DataTransformation:
     }
     
     def convert(self):
-        dataset_samsun = load_from_disk(self.config.data_path)
-        dataset_samsun_pt = dataset_samsun.map(self.convert_examples_to_features, batched=True)
-        dataset_samsun_pt.save_to_disk(os.path.join(self.config.root_dir, "samsun_dataset"))
+        dataset_samsum = load_from_disk(self.config.data_path)
+        dataset_samsum_pt = dataset_samsum.map(self.convert_examples_to_features, batched=True)
+        dataset_samsum_pt.save_to_disk(os.path.join(self.config.root_dir, "samsum_dataset"))
